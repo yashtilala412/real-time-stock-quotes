4782a744e9ad7e1fd4fb3634d41110ebd3c1c1b8
yash <yashtilala412@gmail.com>
2024-08-09 11:46:54 +0530

    controller added

--- src/main/java/com/example/stockservice/controller/StockController.java
+++ src/main/java/com/example/stockservice/controller/StockController.java
@@ -1,5 +1,29 @@
 package com.example.stockservice.controller;
 
+import com.example.stockservice.model.StockQuote;
+import com.example.stockservice.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/quotes")
 public class StockController {
-    
+
+    @Autowired
+    private StockService stockService;
+
+    @GetMapping("/{symbol}")
+    public ResponseEntity<StockQuote> getQuoteBySymbol(@PathVariable String symbol) {
+        StockQuote quote = stockService.getQuote(symbol);
+        return ResponseEntity.ok(quote);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StockQuote>> getBatchQuotes(@RequestParam List<String> symbols) {
+        List<StockQuote> quotes = stockService.getBatchQuotes(symbols);
+        return ResponseEntity.ok(quotes);
+    }
 }
