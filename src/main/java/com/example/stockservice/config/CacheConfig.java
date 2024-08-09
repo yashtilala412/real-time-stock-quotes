69d273986158d34462bbd1c0204c4636110d0ba9
yash <yashtilala412@gmail.com>
2024-08-09 11:49:42 +0530

    cache config

--- src/main/java/com/example/stockservice/config/CacheConfig.java
+++ src/main/java/com/example/stockservice/config/CacheConfig.java
@@ -1,5 +1,20 @@
 package com.example.stockservice.config;
 
+import org.springframework.boot.autoconfigure.cache.CacheProperties.Caffeine;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
 public class CacheConfig {
-    
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(100);
+    }
 }
