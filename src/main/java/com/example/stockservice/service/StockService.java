4927a6b572718641d7e1adef422a3b5f3e6508cb
yash <yashtilala412@gmail.com>
2024-08-09 11:45:57 +0530

    service layer added

--- src/main/java/com/example/stockservice/service/StockService.java
+++ src/main/java/com/example/stockservice/service/StockService.java
@@ -1,5 +1,28 @@
 package com.example.stockservice.service;
 
+import com.example.stockservice.client.StockApiClient;
+import com.example.stockservice.model.StockQuote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class StockService {
-    
+
+    @Autowired
+    private StockApiClient stockApiClient;
+
+    @Cacheable("quotes")
+    public StockQuote getQuote(String symbol) {
+        return stockApiClient.getQuote(symbol);
+    }
+
+    public List<StockQuote> getBatchQuotes(List<String> symbols) {
+        return symbols.stream()
+                .map(this::getQuote)
+                .collect(Collectors.toList());
+    }
 }
