6d4ee6468af125a72126db9b138a24607381301c
yash <yashtilala412@gmail.com>
2024-08-09 11:22:33 +0530

    first commit

---
+++ src/main/java/com/example/stockservice/StockserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockserviceApplication.class, args);
+	}
+
+}
